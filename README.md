51dc09208c79a27e9a5c48af7104d09c20c86fda
Jin Lianyuan <jinlianyuan@192.168.0.107>
2019-06-02 19:48:28 +0800

    modify interface

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## termGenerator.py
 
 该文件的主要作用在于原始文档的处理，Result是结果输出，形式是python的list。再次读入的时候使用python的eval函数即可。
+
+### 提供的一些方法
+
+* get_tokens_from_string 返回将string词条化以后的list，输入为一个string
+* get_tokens_from_file 返回将文件内容词条化以后的list，输入为一个文件路径
+* preprocessing 将一个目录下面的所有文件词条化
--- termGenerator.py
+++ termGenerator.py
@@ -5,37 +5,50 @@ from nltk.corpus import stopwords
 counter = 0
 stopword_list = stopwords.words('english')
 def preprocessing(dir_addr='./Reuters',output_addr='./Result',stemming='False'):
-    Punctuation = list('''!@#$%^&*()_+-=[]\;',./{}|:"<>?''')
     file_list = os.listdir(dir_addr)
     for file_addr in file_list:
-        with open(dir_addr + '/' + file_addr) as f, open(output_addr+'/'+file_addr.split('.')[0]+'.txt','w+') as f_out:
-            res = handle_file(f)
-            for symbol in Punctuation:
-                try:
-                    while True:
-                        res.remove(symbol)
-                except ValueError:
-                    pass
-            for stop_word in stopword_list:
-                try:
-                    while True:
-                        res.remove(stop_word)
-                except ValueError:
-                    pass
-            f_out.write(str(res))
+        with open(output_addr+'/'+file_addr.split('.')[0]+'.txt','w+') as f_out:
+            f_out.write(str(get_tokens_from_file(dir_addr + '/' + file_addr)))
+
+def get_tokens_from_string(content):
+    Punctuation = list('''!@#$%^&*()_+-=[]\;',./{}|:"<>?''')
+    res = nltk.word_tokenize(content)
+    for symbol in Punctuation:
+        try:
+            while True:
+                res.remove(symbol)
+        except ValueError:
+            pass
+    for stop_word in stopword_list:
+        try:
+            while True:
+                res.remove(stop_word)
+        except ValueError:
+            pass    
+    return res 
+
+def get_tokens_from_file(file_addr):
+    with open(file_addr) as f:
+        return get_tokens_from_string(handle_file(f))
 
 def handle_file(f):
-    global counter
-    counter+=1
+    # global counter
+    # counter+=1
     try:
         content = f.read()
-        if counter < 5:
-            print(content)
+        # if counter < 5:
+        #     print(content)
     except UnicodeDecodeError:
-        print(counter)
-    return nltk.word_tokenize(content)
+        print(content)
+    return content
+
+def test():
+    print(get_tokens_from_string('jly jly dqwdh dwq dw q dwq'))
+    print(get_tokens_from_file('./Reuters/1.html'))
 
 if __name__ == '__main__':
+    pass 
+  #  test()
 #    print(os.listdir('./Reuters'))
-    preprocessing()
-    print('counter =',counter)
+    #preprocessing()
+    # print('counter =',counter)
